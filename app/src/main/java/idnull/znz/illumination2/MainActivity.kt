78d48358494153d56acdeefc272cdb583a6c240c
KBaltabayev <KBaltabayev@one.kz>
2022-09-17 17:51:47 +0600

    Added menu

--- app/src/main/java/idnull/znz/illumination2/MainActivity.kt
+++ app/src/main/java/idnull/znz/illumination2/MainActivity.kt
@@ -6,6 +6,10 @@ import android.view.View
 import androidx.appcompat.widget.Toolbar
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import idnull.znz.illumination2.utils.AppPreference
 import idnull.znz.illumination2.databinding.ActivityMainBinding
 
@@ -14,27 +18,42 @@ class MainActivity : AppCompatActivity() {
     lateinit var navController: NavController
     private lateinit var mToolbar: Toolbar
     private lateinit var binding: ActivityMainBinding
+    private lateinit var bottomNavView: BottomNavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         AppPreference.getPreference(this)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        bottomNavView = findViewById(R.id.bottomNavView)
+
         mToolbar = binding.toolbar
         navController = Navigation.findNavController(this, R.id.nav_host_fragment)
 
+        bottomNavView.setupWithNavController(navController)
+
         setSupportActionBar(mToolbar)
-        title = "Raion"
+//        title = "Raion"
 
         navController.addOnDestinationChangedListener { controller, destination, arguments ->
             if (destination.id == R.id.splashFragment) {
                 mToolbar.visibility = View.GONE
+                bottomNavView.visibility = View.GONE
             }
             else if (destination.id == R.id.loginFragment) {
                 mToolbar.visibility = View.GONE
+                bottomNavView.visibility = View.GONE
             }
-            else {
+            else if (destination.id == R.id.chatFragment) {
                 mToolbar.visibility = View.VISIBLE
+                bottomNavView.visibility = View.VISIBLE
+                mToolbar.title = "Chat"
+            }
+            else if (destination.id == R.id.clanFragment) {
+                mToolbar.visibility = View.VISIBLE
+                bottomNavView.visibility = View.VISIBLE
+                mToolbar.title = "Clan"
             }
         }
     }
---
+++ app/src/main/java/idnull/znz/illumination2/presentation/clan/ClanFragment.kt
@@ -0,0 +1,14 @@
+package idnull.znz.illumination2.presentation.clan
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import idnull.znz.illumination2.R
+
+class ClanFragment: Fragment(R.layout.clan_fragment) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
